0719c0ecc21e469311c44915c2c5ccee724c5bd9
Vitor Akio Inoue <vitor_akio@hotmail.com>
2025-06-22 03:28:58 -0600

    render http

--- Program.cs
+++ Program.cs
@@ -24,7 +24,10 @@ app.UseCors();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseAuthorization();
 app.MapControllers();
 
