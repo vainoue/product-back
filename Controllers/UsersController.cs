1b3c3a0684a32164d1a335c72466fa9d9d979dec
Vitor Akio Inoue <vitor_akio@hotmail.com>
2025-06-24 10:08:01 -0600

    case sensitive

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -19,9 +19,13 @@ public class UsersController : ControllerBase
         if (string.IsNullOrWhiteSpace(user.Username) || string.IsNullOrWhiteSpace(user.Password))
             return BadRequest("Username and password are required");
 
-        var existUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username);
+        var lowerUsername = user.Username.ToLower();
+
+        var existUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == lowerUsername);
         if (existUser != null)
             return BadRequest("Username already exists");
+
+        user.Username = lowerUsername;
 
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
@@ -31,7 +35,7 @@ public class UsersController : ControllerBase
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] User user)
     {
-        var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username && u.Password == user.Password);
+        var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username.ToLower() && u.Password == user.Password);
         if (dbUser == null)
             return Unauthorized(new { message = "Invalid username or password" });
 
